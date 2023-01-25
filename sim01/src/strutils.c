5f4096b480b94aec8885fe9547a6f34d3afde392
montaquila <akiel@akiel.org>
2023-01-24 23:21:42 -0700

    updating string utils

--- sim01/src/strutils.c
+++ sim01/src/strutils.c
@@ -1,9 +1,10 @@
 /**
- * String related utilities implementations
+ * String related utilities implementations many of which are from 
  */
 #include "strutils.h"
 
 
+// re-implementation of strcmp()
 int str_cmp(char *str_a, char *str_b)
 {
     int diff, index;
@@ -23,14 +24,51 @@ int str_cmp(char *str_a, char *str_b)
     return (str_len(str_a) - str_len(str_a));
 }
 
-_Bool str_constrained(FILE *inStream,
+/**
+ * re-implementatoin of strstr, finds a string within string 
+ * returning the index
+ */
+int str_substr(char *test_str,char *search_substr)
+{
+  int iVar1;
+  int testStrLen;
+  int internalIndex;
+  int searchIndex;
+  int masterIndex;
+  
+  iVar1 = str_len(test_str);
+  masterIndex = 0;
+  do 
+  { 
+      if (iVar1 <= masterIndex) {
+          return SUBSTRING_NOT_FOUND;
+    }
+    internalIndex = masterIndex;
+    searchIndex = 0;
+    while ((internalIndex <= iVar1 && 
+                (test_str[internalIndex] == 
+                 search_substr[searchIndex]))) {
+      internalIndex++;
+      searchIndex++;
+      if (search_substr[searchIndex] == NULL_CHAR) {
+        return masterIndex;
+      }
+    }
+    masterIndex = masterIndex + 1;
+  } while( true );
+
+
+}
+
+// function for reading from input file stream give contraints
+_Bool str_cstr(FILE *inStream,
                     _Bool clearLeadingNonPrintable,
                     _Bool clearLeadingSpace,
                     _Bool stopAtNonPrintable,
                     char delimiter,
                     char *capturedString)
 {
-    _Bool _Var1;
+    _Bool ret;
     int index;
     int intChar;
 
@@ -50,7 +88,7 @@ _Bool str_constrained(FILE *inStream,
   
     if (intChar == EOF) 
     {
-        _Var1 = false;
+        ret = false;
     }
     else 
     {
@@ -65,11 +103,28 @@ _Bool str_constrained(FILE *inStream,
             capturedString[index] = NULL_CHAR;
             intChar = fgetc((FILE *)inStream);
         }
-        _Var1 = true;
+        ret = true;
     }
-    return _Var1;
+    return ret;
 }
 
+// re-implementation of strcpy()
+void str_cpy(char *dest_str, char *src_str) 
+{
+    int index = 0;
+
+    if (dest_str != src_str)
+    {
+        while ((src_str[index] != NULL_CHAR && (index < MAX_STR_LEN))) 
+        {
+            dest_str[index] = src_str[index];
+            index++;
+            dest_str[index] = NULL_CHAR;
+        }
+    }
+}
+
+// re-implementation of strlen()
 int str_len(char *str_a) 
 {
     int index;
@@ -82,6 +137,4 @@ int str_len(char *str_a)
     }
     return index;
 }
-
-
 
--- sim01/src/strutils.h
+++ sim01/src/strutils.h
@@ -19,23 +19,28 @@ int str_cmp( char *str_a, char *str_b );
 //void str_concat
 
 /**
- * re-implementation of strcpy()
+ * re-implementatoin of strstr, finds a string within
+ * a string
  */
-//void str_cpy(char *dest_str, char *src_str);
-
-//void str_subfind
+int str_substr(char *test_str,char *search_substr);
 
 //void str_subget
 
 /**
  * function for reading from input file stream give contraints
  */
-_Bool str_constrained(FILE *inStream,
+_Bool str_cstr(FILE *inStream,
                     _Bool clearLeadingNonPrintable,
                     _Bool clearLeadingSpace,
                     _Bool stopAtNonPrintable,
                     char delimiter,
                     char *capturedString);
+
+/**
+ * re-implementation of strcpy()
+ */
+void str_cpy(char *dest_str, char *src_str);
+
 /**
  * re-implementation of strlen()
  */
